aceb4c54cd31352b70c5682c80459a6c4f06065f
jeziellopes <jeziellopes@gmail.com>
2022-08-01 17:26:30 -0300

    add search flag and screen preview

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ export const Home = () => {
   const { loader } = useInfiniteScroll({ callback: fetchCompanies, loading })
 
   return (
-    <Layout>
+    <Layout withSearch>
       <CS.Grid>
         {companies
           .filter(
